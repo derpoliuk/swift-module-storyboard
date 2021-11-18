3b51a4becb82fa5e3963d69a1acfc43bc39f47f4
Stanislav <derpoliuk@gmail.com>
2021-11-18 15:05:58 +0200

    Use MyPackage in ViewController

--- Swift Module Storyboard/Swift Module Storyboard/ViewController.swift
+++ Swift Module Storyboard/Swift Module Storyboard/ViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import MyPackage
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let viewController = MyPackage.createViewController()
+        present(viewController, animated: true)
     }
 
 
