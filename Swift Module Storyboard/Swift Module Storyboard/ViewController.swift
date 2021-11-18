9126c8cd78eed48c6ec4e38e6528886d392cf7ef
Stanislav <derpoliuk@gmail.com>
2021-11-18 13:50:53 +0200

    Add empty Xcode project

---
+++ Swift Module Storyboard/Swift Module Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Module Storyboard
+//
+//  Created by Stanislav Derpoliuk on 18.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
